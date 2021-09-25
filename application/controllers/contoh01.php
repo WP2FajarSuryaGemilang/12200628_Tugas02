32fb21711e7e2110922a5941f290384493018bbd
WP2FajarSuryaGemilang <sgfajar1@gmail.com>
2021-09-25 15:13:28 +0700

    menambahkan framework

---
+++ application/controllers/contoh01.php
@@ -0,0 +1,13 @@
+<?php
+ class contoh01 extends CI_controller
+ {
+ public function index()
+ {
+  echo"<h1>perkenalan</h1>";
+  echo"Nama saya FAJAR SURYA GEMILANG
+	  saya tinggal di daerah jatiwaras
+	  olahraga yang saya sukai adalah
+	  sepakbola no telp 082118304553":
+	  }
+  }
+?>
